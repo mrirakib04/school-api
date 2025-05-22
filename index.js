33e8dfea93eb96745cbee0e3d35104030bb74dde
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-05-22 20:53:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import geolib from "geolib";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3030;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.bfqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+
+    // Connections
+    const database = client.db("school_api");
+    const schoolssCollection = database.collection("schools");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("School APIs server");
+});
+
+app.listen(port, () => {
+  console.log(`School APIs server listening on port ${port}`);
+});
